ac7347564886dcf07c6dc96db2e37114c761dc94
ネコリアン <153508475+necolian@users.noreply.github.com>
2024-08-16 13:29:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(3000, '0.0.0.0', () => { 
+    console.log("port is open");
+});
