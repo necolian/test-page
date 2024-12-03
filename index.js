ead4c551ea9c28540fc4e64722caf87683115d00
ネコリアン <153508475+necolian@users.noreply.github.com>
2024-12-03 04:29:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine","ejs");
 
 app.get('/', (req, res) => {
