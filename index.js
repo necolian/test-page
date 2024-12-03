c954a5767dcbac8d4caacd6899138c3ccab0a31c
ネコリアン <153508475+necolian@users.noreply.github.com>
2024-12-03 05:23:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine","ejs");
 
 app.get('/', (req, res) => {
-    var message = process.env.MESSAGE;
-    var h1 = process.env.H1;
-    if (!message){
-        message = "今のところ何か追加＆更新する予定はNothing";
-    }
-    if (!h1){
-        h1 = "テストページだお☆";
-    }
+    var message = process.env.MESSAGE || "今のところ何か追加＆更新する予定はNothing";
+    var h1 = process.env.H1 || "テストページだお☆";
     res.render("index.ejs",{h1: h1, message: message});
 });
 
